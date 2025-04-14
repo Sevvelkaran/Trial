4f53dd64a1d0e7239c0823c1115cd3a88384517f
thouxzzh <2k21cse167@gmail.com>
2025-04-14 22:32:56 +0530

    2 nd Commit

--- Cucumber_!/src/test/java/com/Runner/Runner.java
+++ Cucumber_!/src/test/java/com/Runner/Runner.java
@@ -13,14 +13,14 @@ package com.Runner;
 	    }
 	    ,
 	    features = {
-	       //"src/test/resources/com/features/A_Login.feature",
-	       //"src/test/resources/com/features/PIM.feature",
-//	       "src/test/resources/com/features/TimePunchoutDashboard.feature",
-	       //"src/test/resources/com/features/Configuration.feature",
-	     //"src/test/resources/com/features/Performance.feature",
+	       "src/test/resources/com/features/A_Login.feature",
+	       "src/test/resources/com/features/PIM.feature",
+	       "src/test/resources/com/features/TimePunchoutDashboard.feature",
+	       "src/test/resources/com/features/Configuration.feature",
+	     "src/test/resources/com/features/Performance.feature",
 	    "src/test/resources/com/features/Performance2.feature",
-	  //"src/test/resources/com/features//Buzz.feature",
-	    //"src/test/resources/com/features//Admin.feature"
+	  "src/test/resources/com/features//Buzz.feature",
+	    "src/test/resources/com/features//Admin.feature"
 	    },
 	    glue = "com.Definations",
 	    monochrome = false,
